7ac698131d0bc34872ab42ff2b1d71c4468d630f
Chris Burton <chris@fullstackcaveman.com>
2021-09-18 00:32:08 -0600

    gets saved pic cards set up

---
+++ src/components/elements/LikedPicCard.jsx
@@ -0,0 +1,32 @@
+import { useHistory } from 'react-router-dom';
+import { Button, Card, Image } from 'semantic-ui-react';
+
+const LikedPicCard = (props) => {
+	const history = useHistory();
+	const { likedPic, handleDelete } = props;
+
+	const handleGoToPhoto = () => {
+		history.push(`/${likedPic.date}`);
+	};
+
+	return (
+		<Card>
+			<Image src={likedPic.image} />
+			<Card.Content textAlign='center'>
+				<Card.Header className='card-pic-title'>{likedPic.title}</Card.Header>
+			</Card.Content>
+			<Card.Content extra textAlign='center'>
+				<div className='card-btns'>
+					<Button basic color='green' onClick={handleGoToPhoto}>
+						Go To Photo
+					</Button>
+					<Button basic color='red' onClick={handleDelete}>
+						Delete Photo
+					</Button>
+				</div>
+			</Card.Content>
+		</Card>
+	);
+};
+
+export default LikedPicCard;
