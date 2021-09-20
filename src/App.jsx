cbb14287d2a8dd211119dfd6627eaf3edb662269
Chris Burton <chris@fullstackcaveman.com>
2021-09-19 20:28:49 -0600

    martian spotted!

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
+import { useHistory } from 'react-router';
 import { Route, Switch } from 'react-router-dom';
+import { Image, Popup, Header, Button } from 'semantic-ui-react';
 import CustomPotd from './components/CustomPotd';
 import Background from './components/elements/Background';
 import Home from './components/Home';
 import LikedPics from './components/LikedPics';
+import martian from './images/martian.png';
 
 function App() {
+	const history = useHistory();
+
+	// Sets random search when user clicks the martian!
+	const getRandomDate = () => {
+		const thisYear = new Date().getFullYear();
+		const randomYear = Math.floor(Math.random() * (thisYear - 1996) + 1996);
+		const randomMonth = Math.floor(Math.random() * (12 - 1) + 1).toLocaleString(
+			'en-US',
+			{
+				minimumIntegerDigits: 2,
+				useGrouping: false,
+			}
+		);
+		const randomDay = Math.floor(Math.random() * (28 - 1) + 1).toLocaleString(
+			'en-US',
+			{
+				minimumIntegerDigits: 2,
+				useGrouping: false,
+			}
+		);
+
+		return history.push(`/${randomYear}-${randomMonth}-${randomDay}`);
+	};
+
+	const handleClick = () => {
+		getRandomDate();
+	};
+
 	return (
 		<div className='App'>
 			<Switch>
@@ -12,6 +43,18 @@ function App() {
 				<Route path='/:date' component={CustomPotd} />
 				<Route path='/' component={Home} />
 			</Switch>
+			<Popup
+				trigger={<Image src={martian} id='martian' />}
+				position='left center'
+				hoverable
+				pinned
+			>
+				<Header as='h4'>Feeling Lucky?</Header>
+				<Button color='teal' onClick={handleClick}>
+					See Random Picture
+				</Button>
+			</Popup>
+
 			<Background />
 		</div>
 	);
