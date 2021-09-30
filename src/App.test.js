8159f69abb22d2d322f35bdd17938265c17ffce1
Chris Burton <chris@fullstackcaveman.com>
2021-09-30 13:13:53 -0600

    initial App and Header tests

---
+++ src/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import App from './App';
+
+// const martianBtn = screen.getByText('See Random Picture');
+
+test('should render app without errors', () => {
+	const history = createMemoryHistory();
+	render(
+		<Router history={history}>
+			<App />
+		</Router>
+	);
+});
