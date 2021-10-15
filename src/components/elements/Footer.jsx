e956a3450ee7cf4c2c9ea6936796736cdf5e8d5b
Chris Burton <chris@fullstackcaveman.com>
2021-10-15 14:56:51 -0600

    updates footer

--- src/components/elements/Footer.jsx
+++ src/components/elements/Footer.jsx
@@ -1,14 +1,22 @@
 import { Icon } from 'semantic-ui-react';
 
 const Footer = () => {
+	const thisYear = new Date().getFullYear().toString();
+
 	return (
 		<div className='footer-bottom'>
 			<h2>
-				&copy;FullStackCaveman |{' '}
+				&copy;{thisYear}
+				{/* eslint-disable-next-line */}
+				<a href='https://fullstackcaveman.com' target='_blank' rel='noopener'>
+					{' '}
+					FullStackCaveman
+				</a>{' '}
+				|{' '}
 				<a
 					href='https://github.com/fullstackcaveman'
 					target='_blank'
-					rel='noreferrer nofollow'
+					rel='noreferrer nofollow noopener'
 				>
 					<Icon className='github' /> GitHub
 				</a>{' '}
@@ -16,7 +24,7 @@ const Footer = () => {
 				<a
 					href='https://linkedin.com/in/fullstackcaveman/'
 					target='_blank'
-					rel='noreferrer nofollow'
+					rel='noreferrer nofollow noopener'
 				>
 					{' '}
 					<Icon className='linkedin' /> LinkedIn
